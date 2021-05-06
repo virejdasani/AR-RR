fc28223138f586b34004ee728d1b63c0fef0ef1a
Virej Dasani <dasanivirej@gmail.com>
2021-05-06 18:40:41 +0530

    Fixed a bug where the camera would show a blurry output on android

---
+++ Assets/Scripts/VuforiaFocus.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VuforiaFocus : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var vuforia = VuforiaARController.Instance;
+		vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+		vuforia.RegisterOnPauseCallback(OnPaused);
+	}
+
+	private void OnVuforiaStarted(){
+		CameraDevice.Instance.SetFocusMode(
+		CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+	}
+
+	private void OnPaused(bool paused){
+
+		 if (!paused) // resumed
+		 {
+			 // Set again autofocus mode when app is resumed
+			 CameraDevice.Instance.SetFocusMode(
+			 CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+		 }
+	}
+}
