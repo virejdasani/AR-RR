014eedba08bf5d9e2c48a6174e51ca52147c7a57
Virej Dasani <dasanivirej@gmail.com>
2021-05-08 16:50:03 +0530

    Gave functionality to buttons

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartMenuController : MonoBehaviour
+public class ButtonController : MonoBehaviour
 {
     // This url is entered in the unity editor directly
     public string tutorialUrl;
