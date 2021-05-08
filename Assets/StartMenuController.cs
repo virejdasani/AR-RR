b0a031727f03051fa672c4bb55ca581f5c2553bd
Virej Dasani <dasanivirej@gmail.com>
2021-05-08 16:42:18 +0530

    Added UI

---
+++ Assets/StartMenuController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuController : MonoBehaviour
+{
+    // This url is entered in the unity editor directly
+    public string tutorialUrl;
+
+    // The following functions are all attached to the buttons onClick methods in the editor
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("RickRollScene");
+    }
+
+    public void TutorialButton()
+    {
+        // This will open the url, that we entered in the editor, in the browser
+        Application.OpenURL(tutorialUrl);
+    }
+
+    public void AboutButton()
+    {
+        SceneManager.LoadScene("AboutScene");
+    }
+
+    public void BackToStart()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+
+}
